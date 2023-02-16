0b1e44921c5fc59127b03ea089fa419e05351fef
Adiman007 <adiman.bonnefoy@gmail.com>
2023-02-16 08:02:10 +0100

    update for CloudFlare Deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,3 @@
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
-svelte.config.js
 import adapter from '@sveltejs/adapter-cloudflare';
 
 export default {
